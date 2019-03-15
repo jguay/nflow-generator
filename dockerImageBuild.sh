637f48677882de54de6e054deb8108a4c8d0e71e
Julien Guay <julien@elastic.com>
2019-03-15 22:28:01 +0000

    docker tags

--- dockerImageBuild.sh
+++ dockerImageBuild.sh
@@ -1,1 +1,4 @@
-docker build -t nflow-generator:1.1 .
+docker build -t julienguay/nflow-generator:1.1 .
+docker push julienguay/nflow-generator:1.1
+docker tag julienguay/nflow-generator:1.1 julienguay/nflow-generator:latest
+docker push julienguay/nflow-generator:latest
